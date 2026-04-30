88c790136c4a7a42191978b49134f8130e623e9c
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-30 10:52:55 -0500

    [+] En-Clase/Cajero/ se añade el ejercicio de cajero

---
+++ En-clase/cajero/index.js
@@ -0,0 +1,7 @@
+// simular un cajero automatico, el cual puede realizar diferentes tareas, crear la cuenta debe tener un saldo de 100us:
+// 1. consignar dinero (no se pueden consignar valores negativos)
+// 2. retirar: al retirar solo se puede dejando un saldo del 10% del saldo de la apertura de la cuenta
+// 3. consultar
+// 4. pagar: cobrar el 5%
+// 5. transferir: cobra 1%
+// 6. historial de movimientos ( lista )
