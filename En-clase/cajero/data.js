a172da5119c81f2fe1e1b3cb2b33613a75839906
Sebastian Zarate Pinilla <sebaszarate2005@gmail.com>
2026-04-30 11:55:43 -0500

    [+], se agrega la funcion completa de consignacion

---
+++ En-clase/cajero/data.js
@@ -0,0 +1,57 @@
+export const cuentasBancarias = [
+  {
+    numeroCuenta: "100000001",
+    titular: "Sebastián Zárate Pinilla",
+    documento: "1032456789",
+    tipoCuenta: "Ahorros",
+    moneda: "USD",
+    saldoInicial: 100,
+    saldoActual: 250,
+    activa: true,
+    fechaApertura: "2026-01-15"
+  },
+  {
+    numeroCuenta: "100000002",
+    titular: "María González",
+    documento: "1023456781",
+    tipoCuenta: "Corriente",
+    moneda: "USD",
+    saldoInicial: 100,
+    saldoActual: 480,
+    activa: true,
+    fechaApertura: "2026-02-10"
+  },
+  {
+    numeroCuenta: "100000003",
+    titular: "Carlos Rodríguez",
+    documento: "1045678912",
+    tipoCuenta: "Ahorros",
+    moneda: "USD",
+    saldoInicial: 100,
+    saldoActual: 150,
+    activa: true,
+    fechaApertura: "2026-03-05"
+  },
+  {
+    numeroCuenta: "100000004",
+    titular: "Laura Martínez",
+    documento: "1098765432",
+    tipoCuenta: "Corriente",
+    moneda: "USD",
+    saldoInicial: 100,
+    saldoActual: 920,
+    activa: true,
+    fechaApertura: "2026-03-20"
+  },
+  {
+    numeroCuenta: "100000005",
+    titular: "Andrés Pérez",
+    documento: "1012345678",
+    tipoCuenta: "Ahorros",
+    moneda: "USD",
+    saldoInicial: 100,
+    saldoActual: 75,
+    activa: false,
+    fechaApertura: "2026-04-01"
+  }
+];
--- En-clase/cajero/index.js
+++ En-clase/cajero/index.js
@@ -5,3 +5,69 @@
 // 4. pagar: cobrar el 5%
 // 5. transferir: cobra 1%
 // 6. historial de movimientos ( lista )
+
+import { cuentasBancarias } from "./data.js";
+
+
+
+function consignment(amount, destinationAccount) {
+    if (validateNumber(amount) && verifyDestinationAccount(destinationAccount)){
+        let account = getAccount(destinationAccount);
+        return `La consignacion se hizo exitosamente por un valor de: ${amount} \nCon un numero de cuenta: ${account.numeroCuenta} \nTitular de la cuenta: ${account.titular}`
+    }else{
+        return `Error al hacer la consignacion, verifica el numero de cuenta de destino o que ingreses el monto correcto, recueda que no puede ser 0`
+    }
+};
+
+function withdraw (){
+
+};
+
+function createAccount() {
+
+};
+
+function getAccount(numAccount) {
+    for (let account of cuentasBancarias) {
+        if(numAccount === account.numeroCuenta && account.activa){
+            return account
+        }
+    }
+    return null
+};
+
+
+function verifySourceAccount(sourceAccount) {
+    for (let value of cuentasBancarias) {
+        if (sourceAccount === value.numeroCuenta && value.activa) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function verifyDestinationAccount(destinationAccount) {
+    for (let value of cuentasBancarias) {
+        if (destinationAccount === value.numeroCuenta && value.activa) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function validateNumber(num) {
+    if (num < 0){
+        return false;
+    }
+    return true
+};
+
+function validateBalance (num, account){
+    if (num < account.saldoActual){
+        return false;
+    }
+
+    return true;
+}
+
+console.log(consignment(100, "100000002"));
