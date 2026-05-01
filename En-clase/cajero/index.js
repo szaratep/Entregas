c3a5253771ed9e4902b4ce1c907e9d9f7566369d
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-30 19:39:19 -0500

    cambios en cajero

--- En-clase/cajero/index.js
+++ En-clase/cajero/index.js
@@ -1,14 +1,12 @@
 // simular un cajero automatico, el cual puede realizar diferentes tareas, crear la cuenta debe tener un saldo de 100us:
-// 1. consignar dinero (no se pueden consignar valores negativos)
+// 1. consignar dinero (no se pueden consignar valores negativos) --
 // 2. retirar: al retirar solo se puede dejando un saldo del 10% del saldo de la apertura de la cuenta
-// 3. consultar
-// 4. pagar: cobrar el 5%
-// 5. transferir: cobra 1%
+// 3. consultar --
+// 4. pagar: cobrar el 5% --
+// 5. transferir: cobra 1% --
 // 6. historial de movimientos ( lista )
 
 import { cuentasBancarias } from "./data.js";
-
-
 
 function consignment(amount, destinationAccount) {
     if (validateNumber(amount) && verifyDestinationAccount(destinationAccount)){
@@ -19,13 +17,36 @@ function consignment(amount, destinationAccount) {
     }
 };
 
-function withdraw (){
-
+function withdraw (amount, numAccount){
+    
 };
 
-function createAccount() {
-
+function createAccount(idUser) {
+    if (verifyAccountForUserId(idUser)){
+        return 'tu cuenta ha sido creada correctamente'
+    }
+    return 'error al momento de crear tu cuenta'
 };
+
+function pay (amount, account){
+    if(validateNumber(amount) && validateBalance(amount, account)){
+        return `Pago efectuado exitosamente por un valor de ${amount + (amount * 0.05)} (Se cobra el 5%)`
+    }
+    return `pago invalido` 
+}
+
+function transfer (sourceAccount, amount, destinationAccount){
+    if (validateNumber(amount) && verifyDestinationAccount(destinationAccount) && verifySourceAccount(sourceAccount)){
+        let accountSource = getAccount(sourceAccount);
+        let destinationAccount = getAccount(sourceAccount);
+
+        if(validateBalance(amount, accountSource)){
+            return `´Transferencia exitosa: \nValor: ${amount + (amount * 0.01)} (se cobra un 1%) `
+        }
+    }
+
+    return 'transferencia invalida'
+}
 
 function getAccount(numAccount) {
     for (let account of cuentasBancarias) {
@@ -54,6 +75,15 @@ function verifyDestinationAccount(destinationAccount) {
     }
     return false;
 };
+
+function verifyAccountForUserId (id){
+    for (let value of cuentasBancarias){
+        if(value.documento == id){
+            return false
+        }
+    }
+    return true
+}
 
 function validateNumber(num) {
     if (num < 0){
