3f5a4b0439636de2a464d39e14f0f26cd122607b
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-30 19:36:49 -0500

    [+] Cajero/ se agregan las funciones implementadas aun falta, [+] talleres/TALLER-1/ se crea 3 problemas y se soluciona el primero de la pagina hackerRank

---
+++ Talleres/TALLER-1/firstAlgorithm.js
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+// factorial 
+// Resolver valores muy grandes
+// Entrada: un solo numero consistente n
+// restriccion: 1 <= n <= 100
+// salida: imprime factorial 
+
+
+function extraLongFactorial (n){ 
+    if(n >= 1 && n <= 100){
+        let result = 1;
+    
+        for (let i = n; i >= 1; i-- ){
+            result *= i; 
+        }
+        return result;
+    }else {
+        return 'coloca un  numero entre 1 y 100'
+    }
+}
+
+console.log(extraLongFactorial(100));
