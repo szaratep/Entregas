39460d83d2a0373ba5e82d4b270acfb5e940cd56
Sebastian Zarate Pinilla <sebaszarate2005@gmail.com>
2026-05-01 20:27:33 -0500

    [+] secondAlgorithm.js, se añade el ejercicio de la escalera como segundo algoritmo

--- Talleres/TALLER-1/secondAlgorithm.js
+++ Talleres/TALLER-1/secondAlgorithm.js
@@ -1,1 +1,25 @@
 //https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+// escalera n = 4
+//   #
+//  ##
+// ###
+//####
+
+
+function staircase(n) {
+    for (let i = 1; i <= n; i++) {
+        let space = '';
+        let num = '';
+
+        for (let j = 0; j < n - i; j++) {
+            space += ' ';
+        }
+        for (let k = 0; k < i; k++) {
+            num += '#';
+        }
+
+        console.log(space + num);
+    }
+}
+
+staircase(4)
