b530b769dc1314073fffc587fe192e2737f89f1a
Sebastian Zarate Pinilla <sebaszarate2005@gmail.com>
2026-05-01 20:53:27 -0500

    [+] thirdAlgorithm.js se añade el tercer algoritmo, problema de las velas

--- Talleres/TALLER-1/thirdAlgorithm.js
+++ Talleres/TALLER-1/thirdAlgorithm.js
@@ -1,1 +1,22 @@
 //https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+function birthdayCakeCandles(candles) {
+    let cont = 0;
+    let number = 0;
+
+    for (let value of candles){
+        if (value > number){
+            number = value;
+        }
+    }
+
+    for(let value of candles){
+        if (number === value){
+            cont++
+        }
+    }
+
+    return cont;
+}
+
+birthdayCakeCandles([3, 2, 1, 3])
